7d1c478aa05c3a543187124951079c86d8458d88
kaneko656 <nakano656sho@gmail.com>
2017-08-21 18:55:36 +0900

    support smooth scroll

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
     $('.bxslider').removeClass('invisible')
     $('.bxslider').bxSlider({
         auto: true,
@@ -10,4 +10,50 @@ window.addEventListener("load", function () {
         preloadImages: 'visible',
         controls: false
     })
-});
+
+    $(function() {
+
+        // スクロールのオフセット値
+        var offsetY = -10
+        // スクロールにかかる時間
+        var time1 = 180
+        var time2 = 200
+
+
+        // ページ内リンクのみを取得
+        $('a[href^="#"]').click(function() {
+            // 移動先となる要素を取得
+            var target = $(this.hash)
+            if (!target.length) return
+            // 移動先となる値
+            var targetY = target.offset().top
+
+            let y = $(window).scrollTop()
+
+            time1 = Math.abs(targetY - y) / 7
+            time2 = 70 + time1 * 0.3
+
+            // スクロールアニメーション
+            $('html,body').animate({
+                opacity: 0.5,
+                scrollTop: targetY + offsetY,
+            }, {
+                duration: time1,
+                easing: 'swing',
+                complete: function() {
+                    $('html,body').animate({
+                        opacity: 1.0,
+                    }, {
+                        duration: time2,
+                        easing: 'swing',
+                    })
+                }
+            })
+
+            // ハッシュ書き換えとく
+            window.history.pushState(null, null, this.hash)
+            // デフォルトの処理はキャンセル
+            return false;
+        });
+    });
+})
